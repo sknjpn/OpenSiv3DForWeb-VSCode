c2e98e7c9ef4c72c8011963f7328d6cdee736529
sknjpn <sknjpn@gmail.com>
2022-10-10 06:57:28 +0900

    add auto resize example

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,8 @@
 #include <Siv3D.hpp>
 
+EM_JS(int, GetCanvasWidth, (), { return canvas.width; });
+EM_JS(int, GetCanvasHeight, (), { return canvas.height; });
+
 void Main()
 {
     // resourcesから削除しているため、絵文字及びPrintはASCIIを除いて使えない。
@@ -12,6 +15,8 @@ void Main()
 
     while (System::Update())
     {
+        Window::Resize(GetCanvasWidth(), GetCanvasHeight());
+
         font(U"Hello, Siv3D!").drawAt(Scene::Center(), Palette::Black);
 
         Circle{Cursor::Pos(), 40}.draw(ColorF{1, 0, 0, 0.5});
